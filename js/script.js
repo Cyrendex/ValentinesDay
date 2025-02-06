acbfb55eea1938300967885ab8c51bb6b9230611
Eren Unsal <aaronunsal@gmail.com>
2025-02-06 00:52:32 -0800

    Update first gif and make yes growth adjustable

--- js/script.js
+++ js/script.js
@@ -8,6 +8,9 @@ let gif = document.getElementById("gif");
 let noTexts = ["Are you sure?", "Think again.", "Last chance!", "No way!", "Baby please!", "Don't do this!", "Click Yes!", "You're breaking my heart!", "You monster!", "Say Yes!"];
 let noClickCount = 0;
 
+// Adjustable growth amount
+const GROWTH_AMOUNT = 40; // Change this value to adjust the growth
+
 // Handle "No" button click
 noButton.addEventListener("click", () => {
     if (noClickCount < noTexts.length) {
@@ -17,8 +20,10 @@ noButton.addEventListener("click", () => {
 
     // Increase the size of the Yes button
     let currentSize = parseInt(window.getComputedStyle(yesButton).fontSize);
-    yesButton.style.fontSize = (currentSize + 10) + "px";
-    yesButton.style.padding = (parseInt(yesButton.style.padding || 10) + 5) + "px";
+    let currentPadding = parseInt(window.getComputedStyle(yesButton).padding);
+
+    yesButton.style.fontSize = (currentSize + GROWTH_AMOUNT) + "px";
+    yesButton.style.padding = (currentPadding + GROWTH_AMOUNT / 2) + "px";
 });
 
 // Handle "Yes" button click
